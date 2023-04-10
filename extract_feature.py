b1e9517843cf5333cb21d7495466ed38c4b730e4
whq <whq13600717@outlook.com>
2023-04-10 13:11:49 +0800

    towhee api升级为最新接口

--- extract_feature.py
+++ extract_feature.py
@@ -1,9 +1,7 @@
 import os
 
 import pandas as pd
-import towhee
-from towhee.dc2 import ops, pipe
-from pymilvus import Collection
+from towhee.dc2 import ops, pipe, DataCollection
 from config import MilvusConfig, local_milvus_config
 from load_data import MSRVTT
 
@@ -12,58 +10,114 @@ def get_device():
     # 检查cuda命令
     cmd = 'which nvcc > /dev/null'
     result = os.system(cmd)
-    print(f"CUDA is exist ? {result == 0}")
+    print(f"CUDA is {'not' if result != 0 else ''} exist ?")
     return 'cuda:0' if result == 0 else 'cpu'
 
 
 class VideoSearch:
 
-    def __init__(self, dataset: MSRVTT, collection: Collection, config: MilvusConfig = local_milvus_config, device: str = get_device()):
+    def __init__(self, dataset: MSRVTT,
+                 config: MilvusConfig = local_milvus_config,
+                 device: str = get_device()):
         self.device = device
         self._dataset = dataset
-        self._collection = collection
         self._config = config
 
-    def search_videos(self, search_key: list[str], show_num=10) -> list[pd.DataFrame]:
+    def search_videos(self, search_key: list[str], show_num=10) -> list[dict[str]:list]:
         search_func = (
             pipe.input('sentence_list')
             .map('sentence_list', 'sentence', lambda x: x)
-            .map('sentence', 'vec', ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='text', device=self.device))
-            .map('vec', 'rows', ops.ann_search.milvus_client(host=self._config.host, port=self._config.port, collection_name=self._config.collection_name, limit=show_num))
-            .map('rows', 'videos_path', lambda rows: (os.path.join(self._dataset.video_path, 'video' + str(r[0]) + '.mp4') for r in rows))
+            .map('sentence', 'vec',
+                 ops.video_text_embedding.clip4clip(model_name='clip_vit_b32',
+                                                    modality='text',
+                                                    device=self.device))
+            .map('vec', 'rows',
+                 ops.ann_search.milvus_client(host=self._config.host,
+                                              port=self._config.port,
+                                              collection_name=self._config.collection_name,
+                                              limit=show_num))
+            .map('rows', 'videos_path',
+                 lambda rows: (os.path.join(self._dataset.video_path,
+                                            'video' + str(r[0]) + '.mp4') for r in rows))
             .output('videos_path')
         )
 
-        return [search_func(key).get()[0] for key in search_key]
+        return [{key: search_func(key).get()[0]} for key in search_key]
 
-    def extract_feature_all_video(self, num_samples: int = 12, store_batch: int = 30):
-        (
-            towhee.from_df(self._dataset.csv_file_content).unstream()
-            .video_decode.ffmpeg['video_path', 'frames'](sample_type='uniform_temporal_subsample',
-                                                         args={'num_samples': num_samples})
-            .runas_op['frames', 'frames'](func=lambda x: [y for y in x])
-            .video_text_embedding.clip4clip['frames', 'vec'](model_name='clip_vit_b32',
-                                                             modality='video', device=self.device)
-            .to_milvus['id', 'vec'](collection=self._collection, batch=store_batch)
+    def extract_feature_all_video(self, num_samples: int = 12):
+        def range_pd(data: pd.DataFrame):
+            for row in data.itertuples(index=False):
+                yield getattr(row, 'id'), getattr(row, 'video_path')
+
+        extract_func = (
+            pipe.input('csv_content')
+            .flat_map('csv_content', ('id', 'video_path'), range_pd)
+            .map('video_path', 'frames',
+                 ops.video_decode.ffmpeg(sample_type='uniform_temporal_subsample',
+                                         args={'num_samples': num_samples}))
+            .map('frames', 'vec',
+                 ops.video_text_embedding.clip4clip(model_name='clip_vit_b32',
+                                                    modality='video',
+                                                    device=self.device))
+            .map(('id', 'vec'), (),
+                 ops.ann_insert.milvus_client(host=self._config.host,
+                                              port=self._config.port,
+                                              collection_name=self._config.collection_name))
+            .output()
         )
+        extract_func(self._dataset.csv_file_content.loc[:, ['id', 'video_path']])
 
     def get_hit_ratio(self, show_num: int = 10) -> pd.DataFrame:
-        dc = (
-            towhee.from_df(self._dataset.csv_file_content).unstream()
-            .video_text_embedding.clip4clip['sentence', 'text_vec'](model_name='clip_vit_b32',
-                                                                    modality='text', device=self.device)
-            .milvus_search['text_vec', 'top10_raw_res'](collection=self._collection, limit=show_num)
-            .runas_op['video_id', 'ground_truth'](func=lambda x: [int(x[-4:])])
-            .runas_op['top10_raw_res', 'top1'](func=lambda res: [x.id for i, x in enumerate(res) if i < 1])
-            .runas_op['top10_raw_res', 'top5'](func=lambda res: [x.id for i, x in enumerate(res) if i < 5])
-            .runas_op['top10_raw_res', 'top10'](func=lambda res: [x.id for i, x in enumerate(res) if i < 10])
-        )
-        benchmark = (
-            dc.with_metrics(['mean_hit_ratio', ])
-            .evaluate['ground_truth', 'top1'](name='recall_at_1')
-            .evaluate['ground_truth', 'top5'](name='recall_at_5')
-            .evaluate['ground_truth', 'top10'](name='recall_at_10')
-            .report()
+        def mean_hit_ratio(actual, *predicteds):
+            rets = []
+            for predicted in predicteds:
+                ratios = []
+                for act, pre in zip(actual, predicted):
+                    hit_num = len(set(act) & set(pre))
+                    ratios.append(hit_num / len(act))
+                rets.append(sum(ratios) / len(ratios))
+            return rets
+
+        def get_label_from_raw_data(data):
+            return [item[0] for item in data]
+
+        def range_pd(data: pd.DataFrame):
+            for row in data.itertuples(index=False):
+                yield getattr(row, 'id'), getattr(row, 'sentence')
+
+        dc_search = (
+            pipe.input('csv_content')
+            .flat_map('csv_content', ('id', 'sentence'), range_pd)
+            .map('sentence', 'vec',
+                 ops.video_text_embedding.clip4clip(model_name='clip_vit_b32',
+                                                    modality='text',
+                                                    device=self.device))
+            .map('vec', 'top10_raw_res',
+                 ops.ann_search.milvus_client(host=self._config.host,
+                                              port=self._config.port,
+                                              collection_name=self._config.collection_name,
+                                              limit=show_num))
+            .map('top10_raw_res', ('top1', 'top5', 'top10'), lambda x: (x[:1], x[:5], x[:10]))
+            .map('id', 'ground_truth', lambda x: x)
+            .output('id', 'sentence', 'ground_truth', 'top1', 'top5', 'top10')
         )
 
-        return benchmark
+        ev = (
+            pipe.input('dc_data')
+            #
+            .flat_map('dc_data', 'data', lambda x: x)
+            .map('data', ('ground_truth', 'top1', 'top5', 'top10'),
+                 lambda x: ([x['ground_truth']],
+                            get_label_from_raw_data(x.top1),
+                            get_label_from_raw_data(x.top5),
+                            get_label_from_raw_data(x.top10))
+                 )
+            .window_all(('ground_truth', 'top1', 'top5', 'top10'),
+                        ('top1_mean_hit_ratio', 'top5_mean_hit_ratio', 'top10_mean_hit_ratio'), mean_hit_ratio)
+            .output('top1_mean_hit_ratio', 'top5_mean_hit_ratio', 'top10_mean_hit_ratio')
+        )
+
+        ret = dc_search(self._dataset.csv_file_content.loc[:, ['id', 'sentence']])
+        benchmark = ev(DataCollection(ret))
+
+        return benchmark.get()
--- main.py
+++ main.py
@@ -15,21 +15,23 @@ if __name__ == '__main__':
     config = local_milvus_config
 
     # 创建MilvusCollection对象
+    # 这个操作只需要执行一次，用来创建collection集合就可以了
     with MilvusCollection.new(config) as milvus:
         # 添加数据到Milvus数据库中
-        collection = milvus.create_milvus_collection()
+        milvus.create_milvus_collection()
 
-        # 创建VideoSearch处理视频和文本特征信息
-        search = VideoSearch(dataset, collection, config)
+    # 创建VideoSearch处理视频和文本特征信息
+    search = VideoSearch(dataset, config)
 
-        # 抽取特征
-        search.extract_feature_all_video()
-        # 统计测试集所有的文本特征，计算模型准确率
-        # result = search.get_hit_ratio()
+    # 抽取特征，这个操作也只需要执行一次，抽取一次信息即可
+    search.extract_feature_all_video()
+    # 统计测试集所有的文本特征，计算模型准确率
+    # result = search.get_hit_ratio()
+    # print(f"Benchmark: {result}")
 
-        # 通过一个文本获取相关视频信息
-        search_key = ['baseball player', 'looking at data']
-        result = search.search_videos(search_key=search_key)
-        print(f'Search {search_key} result:\n{result}')
+    # 通过一个文本获取相关视频信息
+    search_key = ['baseball player', 'looking at data']
+    result = search.search_videos(search_key=search_key)
+    print(f'Search {search_key} result:\n{result}')
 
 
